02b4ad4c12504eede6fdb88d56449b44c2da88f4
David A. Wheeler <dwheeler@dwheeler.com>
2020-11-23 22:58:04 -0500

    Update source code with "cargo fix"
    Signed-off-by: David A. Wheeler <dwheeler@dwheeler.com>

--- src/export.rs
+++ src/export.rs
@@ -55,7 +55,7 @@ impl error::Error for ExportError {
         }
     }
 
-    fn cause(&self) -> Option<&error::Error> {
+    fn cause(&self) -> Option<&dyn error::Error> {
         match *self {
             ExportError::Io(ref err) => Some(err),
             ExportError::Verify(_) => None,
