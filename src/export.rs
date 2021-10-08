2756afb6db36fcc0ce273e203dee65a0392f4a61
Mario Carneiro <di.gama@gmail.com>
2021-10-08 02:26:01 -0700

    Update src/export.rs
    Co-authored-by: tirix <thierry.arnoux@gmx.net>

--- src/export.rs
+++ src/export.rs
@@ -18,7 +18,7 @@ use std::io;
 use std::io::Write;
 use std::str;
 
-/// The error type for [`export::export_mmp`].
+/// The error type for [`export_mmp`].
 #[derive(Debug)]
 pub enum ExportError {
     /// IO Error during write
