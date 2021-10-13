02509601ed65ea239278563f8e298ae56f9847dd
Thierry Arnoux <thierry.arnoux@gmx.net>
2021-10-13 22:25:42 +0800

    Fix for #32

--- src/grammar.rs
+++ src/grammar.rs
@@ -117,7 +117,11 @@ impl GrammarTree {
                             //panic!("Conflict when copying constant grammar branches!");
                         }
                         Entry::Vacant(e) => {
-                            e.insert(next_node.with_reduce(add_reduce));
+                            if stype == SymbolType::Variable {
+                                e.insert(next_node.with_offset_reduce(add_reduce));
+                            } else {
+                                e.insert(next_node.with_reduce(add_reduce));
+                            }
                         }
                     }
                 }
@@ -228,6 +232,17 @@ impl NextNode {
     const fn new_with_reduce_vec(next_node_id: NodeId, leaf_label: ReduceVec) -> Self {
         NextNode {
             next_node_id,
+            leaf_label,
+        }
+    }
+
+    fn with_offset_reduce(&self, reduce: Reduce) -> Self {
+        let mut r = reduce;
+        r.offset += 1;
+        let mut leaf_label = self.leaf_label;
+        leaf_label.insert(0, r);
+        NextNode {
+            next_node_id: self.next_node_id,
             leaf_label,
         }
     }
