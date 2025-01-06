d718d047731e3a9bf3347c9a4aaf130634f33646
Thierry Arnoux <5831830+tirix@users.noreply.github.com>
2025-01-06 10:18:20 +0100

    Check for unknown labels in usage commands

--- metamath-rs/src/usage_tests.rs
+++ metamath-rs/src/usage_tests.rs
@@ -1,4 +1,4 @@
-use crate::diag::Diagnostic::UsageViolation;
+use crate::diag::Diagnostic::{UnknownLabel, UsageViolation};
 use crate::grammar_tests::mkdb;
 use assert_matches::assert_matches;
 
@@ -9,7 +9,7 @@ ax-1 $a |- > a $.
 thm1 $p |- > a $= wa ax-1 $.
 $( $j usage 'thm1' avoids 'ax-1'; $)
 thm2 $p |- > a $= ( ax-1 ) AB $.
-$( $j usage 'thm2' avoids 'ax-1'; $)
+$( $j usage 'thm2' avoids 'ax-1' 'ax-unknown'; $)
 ";
 
 #[test]
@@ -19,7 +19,8 @@ fn test_usage() {
     let usage = db.verify_usage_pass();
     let diags = usage.diagnostics();
 
-    assert_eq!(diags.len(), 2);
+    assert_eq!(diags.len(), 3);
     assert_matches!(diags[0], (_, UsageViolation(..)));
     assert_matches!(diags[1], (_, UsageViolation(..)));
+    assert_matches!(diags[2], (_, UnknownLabel(..)));
 }
